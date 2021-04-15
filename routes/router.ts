d63164afcb911bbebd8811656d827736eebb2e32
Javier <193231@ids.upchiapas.edu.mx>
2021-04-15 12:13:54 -0500

    database connection and gitignore config env

--- routes/router.ts
+++ routes/router.ts
@@ -1,4 +1,17 @@
 import { Router, Request, Response } from 'express';
+import { Pool } from 'pg';
+
+require('dotenv').config();
+
+const pool = new Pool();
+
+pool.connect((err) => {
+    if (err) {
+        return console.error('Error al conectar la base de datos')
+    }else{
+        return console.log('Se ha conectado a la base de datos de K8s')
+    }
+  })
 
 const router = Router();
 
